44fb6973aa4a487c8dfb06ec32aad8cd702204d6
eoyaan <130785505+eoyaan@users.noreply.github.com>
2024-08-30 21:11:10 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é o principal objetivo da inteligência artificial?",
+        alternativas: [
+            {
+                texto: "a) Reproduzir comportamentos humanos",
+                afirmacao: [
+                    "a) Reproduzir comportamentos humanos"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B)Criar novas formas de eletrônicos.",
+                afirmacao: [
+                    "a) Reproduzir comportamentos humanos"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual das seguintes tecnologias é um exemplo de IA?",
+        alternativas: [
+            {
+                texto: "A) Assistentes virtuais (como Alexa ou Siri)",
+                afirmacao: [
+                    "a)Assistentes virtuais (como Alexa ou Siri)",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B)Processadores de texto",
+                afirmacao: [
+                    "a)Assistentes virtuais (como Alexa ou Siri)"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual destas áreas está fortemente ligada ao desenvolvimento da IA??",
+        alternativas: [
+            {
+                texto: "a) Psicologia",
+                afirmacao: [
+                    "B) Ciências da Computação"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B) Ciências da Computação",
+                afirmacao: [
+                    "B)  Ciências da Computação"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a principal diferença entre IA forte e IA fraca?",
+        alternativas: [
+            {
+                texto: "a) A IA forte tem consciência própria, enquanto a IA fraca apenas realiza tarefas específicas",
+                afirmacao: [
+                    "a) A IA forte tem consciência própria, enquanto a IA fraca apenas realiza tarefas específicas"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b) A IA fraca é mais rápida que a IA forte",
+                afirmacao: [
+                    "a) A IA forte tem consciência própria, enquanto a IA fraca apenas realiza tarefas específicas"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual tecnologia de IA permite que máquinas entendam a linguagem humana e conversem com as pessoas?",
+        alternativas: [
+            {
+                texto: "a) Visão computacional",
+                afirmacao: [
+                    "b) Processamento de linguagem natural 'PLN'."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b) Processamento de linguagem natural (PLN)",
+                afirmacao: [
+                    "b) Processamento de linguagem natural (PLN)"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual destes é um risco potencial relacionado ao uso de IA?",
+        alternativas: [
+            {
+                texto: "a) Redução de custos operacionais",
+                afirmacao: [
+                    "B)Preocupações éticas e impacto no mercado de trabalho"
+                ],
+            },
+            {
+                texto: "B) Júpiter",
+                afirmacao: [
+                    "B) Preocupações éticas e impacto no mercado de trabalho"
+                ],
+            },
+        ]
+    },
+];
